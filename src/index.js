e492574bc6b77ed0423e86f0b88bb38a8d623fbb
Thiago Torres <thiagomev@gmail.com>
2022-07-20 03:58:08 -0300

    Add card to pass

--- src/index.js
+++ src/index.js
@@ -9,19 +9,18 @@ const client = new Client({
   ],
 });
 
-client.on('ready', () => {
-	console.log('Bot: Hosting ' + `${client.users.size}` + ' users, in ' + `${client.channels.size}` + ' channels of ' + `${client.guilds.size}` + ' guilds.');
-			client.user.setStatus('online')
-			client.user.setActivity(`Mevzin 💜`, {type: "playing", name: "teste"});
-	});
+client.on("ready", () => {
+  client.user.setStatus("online");
+  client.user.setActivity(`Mevzin 💜`, { type: "playing", name: "teste" });
+});
 
 client.on("messageCreate" || "messageEdit", (message) => {
-	if (message.channelId !== process.env.FARM_CHANNEL_ID) return;
+  if (message.channelId !== process.env.FARM_CHANNEL_ID) return;
   if (message.author.bot) return;
   let dataFarm = message.content.split("\n");
-  console.log(dataFarm);
 
-  if (dataFarm.length != 6 && dataFarm.length != 4){
+  if (dataFarm.length != 6 && dataFarm.length != 4) {
+    message.react("❌");
     const embed = new MessageEmbed()
       .setTitle(`Não foi possível salvar estes dados`)
       .setDescription("Envie os dados em um dos seguintes formatos:")
@@ -35,25 +34,63 @@ client.on("messageCreate" || "messageEdit", (message) => {
             "Quantidade Pólvora\n" +
             "Quantidade Projetil\n" +
             "Quantidade Capsula",
-					inline: true
+          inline: true,
         },
-				{ name: '\u200B', value: '\u200B', inline: true },
+        { name: "\u200B", value: "\u200B", inline: true },
         {
           name: "Somente Pólvora",
-          value:
-            "dd/mm/yyyy\n" +
-            "Nome\n" +
-            "Id\n" +
-            "Quantidade Pólvora",
-						inline: true
-					
+          value: "dd/mm/yyyy\n" + "Nome\n" + "Id\n" + "Quantidade Pólvora",
+          inline: true,
         }
       )
       .setColor("#F91207");
     message.reply({ embeds: [embed] });
-    message.react("❌");
   } else {
-    message.react("✅");
+    if (dataFarm.length == 4) {
+      message.react("✅");
+      const successEmbed = new MessageEmbed()
+        .setColor("#0EF907")
+        .setTitle("Dados salvos com sucesso!")
+        .setAuthor({
+          name: message.author.username,
+          iconURL: message.author.displayAvatarURL(),
+        })
+        .setThumbnail("https://i.imgur.com/2zqiYnJ.png")
+        .addFields({
+          name: "Somente Pólvoras",
+          value:
+            `Data: ${dataFarm[0]} \n`+
+            `Nome: ${dataFarm[1]} \n`+
+            `Id: ${dataFarm[2]} \n`+
+            `Qtd Pólvoras: ${dataFarm[3]} \n`,
+          inline: true,
+        })
+        .setTimestamp()
+      message.reply({ embeds: [successEmbed] });
+    } else if (dataFarm.length == 6) {
+      message.react("✅");
+      const successEmbed = new MessageEmbed()
+      .setColor("#0EF907")
+      .setTitle("Dados salvos com sucesso!")
+      .setAuthor({
+        name: message.author.username,
+        iconURL: message.author.displayAvatarURL(),
+      })
+      .setThumbnail("https://i.imgur.com/2zqiYnJ.png")
+      .addFields({
+        name: "Somente Pólvoras",
+        value:
+          `Data: ${dataFarm[0]} \n`+
+          `Nome: ${dataFarm[1]} \n`+
+          `Id: ${dataFarm[2]} \n`+
+          `Qtd Pólvoras: ${dataFarm[3]} \n`+
+          `Qtd Projeteis: ${dataFarm[4]} \n`+
+          `Qtd Capsulas: ${dataFarm[5]} \n`,
+        inline: true,
+      })
+      .setTimestamp()
+    message.reply({ embeds: [successEmbed] });
+    }
   }
 });
 
